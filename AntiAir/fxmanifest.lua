95182de59eb9366e4bc32add1238228c79842bf2
Scarvixan <scarvixan@gmail.com>
2025-02-14 00:18:59 +1100

    Add files via upload

---
+++ AntiAir/fxmanifest.lua
@@ -0,0 +1,19 @@
+fx_version 'cerulean'
+game 'gta5'
+author 'Scarvixan'
+
+files {
+	'data/caraddoncontentunlocks.meta',
+	'data/carvariations.meta',
+	'data/dlctext.meta',
+	'data/handling.meta',
+	'data/vehicles.meta',
+	'data/*.meta'
+}
+
+data_file 'DLCTEXT_FILE' 'data/dlctext.meta'
+data_file 'HANDLING_FILE' 'data/handling.meta'
+data_file 'VEHICLE_METADATA_FILE' 'data/vehicles.meta'
+data_file 'VEHICLE_VARIATION_FILE' 'data/carvariations.meta'
+data_file 'WEAPONINFO_FILE' 'data/vehicleweapons_def_rim.meta'
+data_file 'WEAPONINFO_FILE' 'data/vehicleweapons_def_ciws.meta'
